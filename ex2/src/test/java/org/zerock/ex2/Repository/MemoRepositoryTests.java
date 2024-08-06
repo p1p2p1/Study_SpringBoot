0678c628ffd27f46087c0f7005364a0da85b6892
p1p2p1 <dbwjdals345@gmail.com>
2024-08-06 15:34:16 +0900

    JPA CRUD Test

--- ex2/src/test/java/org/zerock/ex2/Repository/MemoRepositoryTests.java
+++ ex2/src/test/java/org/zerock/ex2/Repository/MemoRepositoryTests.java
@@ -3,14 +3,68 @@ package org.zerock.ex2.Repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.ex2.Entity.Memo;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 
 @SpringBootTest
 public class MemoRepositoryTests {
     @Autowired
     MemoRepository memoRepository;
 
-    @Test
-    public void testInsertDummies(){
+    //INSERT
+//    @Test
+//    public void testInsertDummies(){
+//        IntStream.rangeClosed(1,100).forEach(i -> {
+//            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+//            memoRepository.save(memo); //save() 메서드
+//        });
+//    }
 
+    //SELECT
+    @Test
+    public void testSelect(){
+//        Long mno = 100L;
+
+//        //findById()
+//        Optional<Memo> result = memoRepository.findById(mno);
+//        System.out.println(result);
+
+//        //findAll()
+//        List<Memo> result = memoRepository.findAll();
+//
+//        for (Memo memo : result) {
+//            System.out.println(memo);
+//        }
+
+//      //count()
+//        System.out.println(memoRepository.count());
+    }
+
+    @Test
+    public void testUpdate(){
+//        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+//
+//        System.out.println(memoRepository.save(memo));
+    }
+    @Test
+    public void testDelete(){
+
+//        Long mno = 100L;
+//        //deleteById()
+//        memoRepository.deleteById(mno);
+//        System.out.println(memoRepository.findById(mno));
+
+        //delete()
+        Memo memo = Memo.builder().mno(99L).build();
+
+        System.out.println(memo);
+
+        memoRepository.delete(memo);
+
+        System.out.println(memoRepository.findById(99L));
     }
 }
