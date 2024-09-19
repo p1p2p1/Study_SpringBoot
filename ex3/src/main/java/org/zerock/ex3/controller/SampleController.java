eee8e416d5c41cecfe835bbea28573f156f875c9
p1p2p1 <dbwjdals345@gmail.com>
2024-09-19 18:49:01 +0900

    querydsl 설정

--- ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -23,7 +23,7 @@ public class SampleController {
         log.info("ex1........");
     }
 
-    @GetMapping("/ex2")
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
             SampleDTO dto = SampleDTO.builder()
@@ -49,5 +49,10 @@ public class SampleController {
     @GetMapping("/ex3")
     public void ex3(){
         log.info("ex3");
+    }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "/exSidebar"})
+    public void exLayout1(){
+        log.info("exLayout1");
     }
 }
